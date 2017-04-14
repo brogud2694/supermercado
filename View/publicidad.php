0ea725bb43760528d38434867d1b27f571ccc42e
OroJackson <OroJackson@192.168.1.3>
2017-04-13 21:02:01 -0600

    Gift

--- View/publicidad.php
+++ View/publicidad.php
@@ -1,10 +1,13 @@
 </section>
-            <aside>
-                <figure>
-                    <a class="fade" href="https://mediacionvirtual.ucr.ac.cr/" target="_blank">
-                        <img src="View/img/mediacionvirtual.png"/>
-                        <figcaption>EN CONSTRUCCI&Oacute;</figcaption>
-                    </a>
-                </figure>
-            </aside>
-        </section>
+<aside>
+    <figure>
+        <a class="fade" href="https://www.worldofwarcraft.com/es-mx" target="_blank">
+            <img 
+                src="https://s-media-cache-ak0.pinimg.com/originals/b7/78/df/b778df6c7a7ff76384f178792f408349.gif" 
+                alt="" class="img_ad" width="300" border="0"
+                >
+            <figcaption>EN CONSTRUCCI&Oacute;</figcaption>
+        </a>
+    </figure>
+</aside>
+</section>
