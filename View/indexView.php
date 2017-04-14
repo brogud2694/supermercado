f64da2ccf17c5fc221be5c3b0d424173d6576f71
OroJackson <OroJackson@192.168.1.3>
2017-04-13 20:14:05 -0600

    Virgen

--- View/indexView.php
+++ View/indexView.php
@@ -3,7 +3,7 @@
 ?>
 <article>
     <p>
-        Este curso le permite al estudiante y la estudiante obtener los conocimientos necesarios para desarrollar aplicaciones comerciales de software haciendo uso de lenguajes de programación de alto nivel, lenguajes como SQL, PHP, HTML, C# y Javascript, ambientes de desarrollo integrados y “frameworks” de aplicaciones.
+        .Este curso le permite al estudiante y la estudiante obtener los conocimientos necesarios para desarrollar aplicaciones comerciales de software haciendo uso de lenguajes de programación de alto nivel, lenguajes como SQL, PHP, HTML, C# y Javascript, ambientes de desarrollo integrados y “frameworks” de aplicaciones.
     </p>
 </article>
 
